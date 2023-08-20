37163d7dd5f2937110504bd8099c1f0a757ccda4
AMIHbKA <bishop0013forum@gmail.com>
2023-08-20 08:15:04 +0300

    done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,9 @@
 const { User } = require("../../models/user");
-const { HttpError } = require("../../utilities");
+const { HttpError, sendEmail } = require("../../utilities");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+
+const { v4: uuidv4 } = require("uuid");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,13 +14,25 @@ const register = async (req, res) => {
     throw HttpError(409, "Email already in use");
   }
 
+  const { BASE_URL } = process.env;
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = uuidv4();
+
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target ="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     email: newUser.email,
